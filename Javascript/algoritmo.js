7b084876ae43cd8d2d15dbaf493c0aad526f484f
jaidernc <jaidernc22@gmail.com>
2022-08-10 14:25:59 -0300

    Algoritmo

--- Javascript/algoritmo.js
+++ Javascript/algoritmo.js
@@ -1,9 +1,24 @@
-let cantidad,precio,descuento,comprar,pagar;
-precio=parseFloat(prompt("Ingrese precio"))
-cantidad = parseFloat(prompt("Ingrese cantidad"))
+debugger
 
-comprar = precio*cantidad;
-descuento = comprar*0.15;
-pagar = comprar
-console.log("El descuento es:"+descuento);
-console.log("El total a pagar es:"+ pagar);
+let seguimos = true
+let operador = 3
+while(seguimos){
+let nota1 = parseInt(prompt("Ingrese la primera nota."))
+let nota2 = parseInt(prompt("Ingrese la segunda nota."))
+let nota3 = parseInt(prompt("Ingrese la tercera nota."))
+let notaFinal = calcular(nota1,nota2,nota3)
+
+if (calcular >= 10){
+    console.log("Felicidades, haz aprobado.",calcular)
+}
+else if (calcular < 7){
+    console.log("Lo sentimos, no haz logrado aprobar.",calcular)
+}
+
+}
+
+function calcular(not1,not2,not3, ope){
+        return ((not1 + not2) + (not3)) / ope
+}
+
+
