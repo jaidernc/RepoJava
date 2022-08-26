f9b99009aa5eb9363a329ed4795783fe2c9fea07
jaidernc <jaidernc22@gmail.com>
2022-08-25 23:43:01 -0300

    Aplicando eventos + JSON y storage

--- Javascript/DOM.js
+++ Javascript/DOM.js
@@ -15,26 +15,117 @@ const prod3 = new Producto("Monitor 14 pulgada", 30000, 24)
 */
 
 
-function cargarProductos (){
+/*function cargarProductos (){
     const Tarjeta = document.getElementById("Tarjetas")
     productos.forEach(producto => {
-        Tarjeta.innerHTML += `<div class="card" style="width: 18rem;">
+        Tarjeta.innerHTML += `  <div class="row">
+                                <div class="col-sm-12 col-md-6 col-xl-4">
+                                <div class="card" style="width: 18rem;">
                                 <img src="..." class="card-img-top" alt="...">
                                 <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <a href="#" class="btn btn-primary agrgar carrito">añadir al carrito</a>
                                 </div>
                                 </div>
-                                 </div>`
+                                </div>
+                                </div>
+                                </div>`
     })
-}
+}*/
 
-cargarProductos ()
+//cargarProductos ()
 
-const titulo = document.querySelector("#Titulo")
+/*const titulo = document.querySelector("#Titulo")
 titulo.addEventListener("mousemove",() => {
     titulo.title = "Prueba de evento mousemove"
 })
+*/
+const containerDiv = document.querySelector(".container")
+const carritoDiv = document.querySelector(".carrito")
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+
+function crearCards (){
+    productos.forEach(prod=>{
+        containerDiv.innerHTML +=  `<div class="row">
+                                    <div class="col-sm-12 col-md-6 col-xl-4">
+                                     <div class="card" style="width: 18rem;">
+                                    <img src="./img/smartphone.jpg" class="card-img-top" alt="imagen de un smartphone">
+                                    <div class="card-body">
+                                    <h5 class="card-title">${prod.Nombre}</h5>
+                                    <p class="card-text">${prod.descripcion}</p>
+                                    <p class="card-text">${prod.precio}</p>
+                                    <p class="card-text">${prod.stock}</p>
+                                    <button class="btn btn-primary carrito" id= "btn-agregar${prod.id}">añadir al carrito</button>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>`   
+    })
+
+    funcionalidadbtn()
+                        
+}
+
+function funcionalidadbtn(){
+    productos.forEach((prod) => {
+        document.querySelector(`#btn-agregar${prod.id}`).addEventListener("click", ()=> {
+            console.log(prod)
+            agregarAlCarrito(prod)
+        })
+    })
+}
+
+function agregarAlCarrito(prod){
+    let existe = carrito.some((productoSome) =>productoSome.id === prod.id)
+    if(existe === false){
+        prod.cantidad                                                                                                                                                                                                              = 1
+        carrito.push(prod)
+    } else{
+        prod.cantidad++
+    }
+    console.log(carrito)
+    reenderizaCarrito ()
+}
 
 
+function reenderizaCarrito (){
+    carritoDiv.innerHTML = ""
+    carrito.forEach((prod) => {
+        carritoDiv.innerHTML += `<div class="row">
+                                <div class="col-sm-12 col-md-6 col-xl-4">
+                                <div class="card" style="width: 18rem;">
+                                <img src="./img/smartphone.jpg" class="card-img-top" alt="imagen de un smartphone">
+                                <div class="card-body">
+                                <h5 class="card-title">${prod.Nombre}</h5>
+                                <p class="card-text">${prod.descripcion}</p>
+                                <p class="card-text">${prod.precio}</p>
+                                <p class="card-text">${prod.stock}</p>
+                                <button class="btn btn-primary carrito" id= "btn-borrar${prod.id}">borrar</button>
+                                </div>
+                                </div>
+                                </div>
+                                </div>
+                                </div>`
+    
+    })
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+    borrarProductos()
+
+}
+
+function borrarProductos(){
+    carrito.forEach((prod) => {
+        document.querySelector(`#btn-borrar${prod.id}`)
+        .addEventListener("click", ()=> {
+            carrito =carrito.filter(
+                (productofilter) =>productofilter.id !== prod.id)
+                    reenderizaCarrito ()
+        })
+    })
+
+}
+crearCards ()
+reenderizaCarrito ()
--- Javascript/arrays.js
+++ Javascript/arrays.js
@@ -1,13 +1,15 @@
-const productos = [{Nombre:"Huawei p9", precio: 54000, descripcion:"smartphone", stock: 24,}, 
-                    {Nombre:"Moto G20", precio: 54000, descripcion:"smartphone", stock: 24,},
-                    {Nombre:"Samsung A20", precio: 54000, descripcion:"smartphone", stock: 24,},
-                    {Nombre:"Samsung A10", precio: 54000, descripcion:"smartphone", stock: 24,},
-                    {Nombre:"Samsung A30", precio: 54000, descripcion:"smartphone", stock: 24,},
-                    {Nombre:"Samsung A50", precio: 54000, descripcion:"smartphone", stock: 24,},
-                    {Nombre:"Samsung A70", precio: 54000, descripcion:"smartphone", stock: 24,},
-                    {Nombre:"Samsung A80", precio: 54000, descripcion:"smartphone", stock: 24,},
-                    {Nombre:"Xiaomi Redmi Note7", precio: 54000, descripcion:"smartphone", stock: 24,},
-                    {Nombre:"Xiaomi Redmi Note8", precio: 54000, descripcion:"smartphone", stock: 24,},
-                    {Nombre:"Xiaomi Mi 9Tpro", precio: 54000, descripcion:"smartphone", stock: 24,},
-                    {Nombre:"HonorX8", precio: 54000, descripcion:"smartphone", stock: 24,}]    
+const productos = [{ Nombre:"Huawei p9", precio: 54000, descripcion:"smartphone", stock: 24,id: 1,}, 
+                    {Nombre:"Moto G20", precio: 54000, descripcion:"smartphone", stock: 24,id: 2,},
+                    {Nombre:"Samsung A20", precio: 54000, descripcion:"smartphone", stock: 24,id: 3,},
+                    {Nombre:"Samsung A10", precio: 54000, descripcion:"smartphone", stock: 24,id: 4,},
+                    {Nombre:"Samsung A30", precio: 54000, descripcion:"smartphone", stock: 24,id: 5,},
+                    {Nombre:"Samsung A50", precio: 54000, descripcion:"smartphone", stock: 24,id: 6,},
+                    {Nombre:"Samsung A70", precio: 54000, descripcion:"smartphone", stock: 24,id: 7,},
+                    {Nombre:"Samsung A80", precio: 54000, descripcion:"smartphone", stock: 24,id: 8,},
+                    {Nombre:"Xiaomi Redmi Note7", precio: 54000, descripcion:"smartphone", stock: 24,id: 9,},
+                    {Nombre:"Xiaomi Redmi Note8", precio: 54000, descripcion:"smartphone", stock: 24,id: 10,},
+                    {Nombre:"Xiaomi Mi 9Tpro", precio: 54000, descripcion:"smartphone", stock: 24,id: 11,},
+                    {Nombre:"HonorX8", precio: 54000, descripcion:"smartphone", stock: 24,id: 12,}]    
                     
+
+
